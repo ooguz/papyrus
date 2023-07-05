1091d71a5c04f06306c0112301d3e63c79d2b990
Özcan Oğuz <ozcan@oyd.org.tr>
2023-07-05 19:37:29 +0300

    Add basic tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,25 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:get/get.dart';
+import 'package:getx_test/getx_test.dart';
+import 'package:papyrus/app/modules/home/controllers/home_controller.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  TestWidgetsFlutterBinding.ensureInitialized();
+  getTest("Route test", widgetTest: (tester) async {
+    expect('/', Get.currentRoute);
   });
+  testObx(
+    'Stepper test',
+    widget: (controller) => Obx(
+      () => Text("Step:${controller.currentStep.value}"),
+    ),
+    controller: HomeController(),
+    test: (e) {
+      expect(find.text("Step:0"), findsOneWidget);
+      expect(e.currentStep.value, 0);
+      e.currentStep.value = 1;
+      expect(e.currentStep.value, 1);
+    },
+  );
 }
